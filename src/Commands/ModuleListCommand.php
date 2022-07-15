bb2f98766709a4e3d58c75482c191773d7866766
tienlv <tienlv@smartosc.com>
2022-07-15 18:33:43 +0700

    update code

---
+++ src/Commands/ModuleListCommand.php
@@ -0,0 +1,91 @@
+<?php
+namespace OctopusOsc\Core\Commands;
+use Illuminate\Console\Command;
+
+class ModuleListCommand extends Command
+{
+    const COLSPAN_1= 30;
+    const COLSPAN_2= 20;
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'module:list';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'List the modules.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(){
+        $modules = [
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ],
+          [
+              "name" => "Module Name",
+              "status" => true
+          ]
+        ];
+        $this->line(str_pad("Module Name", self::COLSPAN_1) . "|".str_pad("Status", self::COLSPAN_2). "|");
+        $this->line(str_pad("", self::COLSPAN_1, "-") . "|".str_pad("", self::COLSPAN_2, "-"). "|");
+        foreach ($modules as $module){
+            $this->line(str_pad($module["name"], self::COLSPAN_1) . "|".str_pad($module["status"] ? "Enable" : "Disable", self::COLSPAN_2). "|");
+        }
+    }
+}
--- src/Providers/CoreServiceProvider.php
+++ src/Providers/CoreServiceProvider.php
@@ -1,11 +1,20 @@
 <?php
+
 namespace OctopusOsc\Core\Providers;
+
 use Illuminate\Support\ServiceProvider;
+use OctopusOsc\Core\Commands\ModuleListCommand;
 
 class CoreServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        dd('Ok');
+////        dd(__DIR__ . "/../../routes/web.php");
+//        $this->loadRoutesFrom(__DIR__ . "/../routes/web.php");
+//        $this->loadViewsFrom(__DIR__ . "/../resources/views", "botble-demo");
+
+        $this->commands([
+            ModuleListCommand::class
+        ]);
     }
 }
