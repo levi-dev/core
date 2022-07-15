fc2643e0e407db6db913dd4dfb3da3f8cca9b0c1
tienlv <tienlv@smartosc.com>
2022-07-15 16:32:16 +0700

    update composer.json

---
+++ src/Providers/CoreServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+namespace OctopusOsc\Core\Providers;
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        dd('Ok');
+    }
+}
